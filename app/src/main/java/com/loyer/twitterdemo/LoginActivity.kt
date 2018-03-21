00a16728ad418c2533a4d73d3d5bf122b614a44d
celikrecep <recep_clk10@hotmail.com>
2018-03-21 23:13:36 +0300

    Handling user login with FirebaseAuth and listView adapter

--- app/src/main/java/com/loyer/twitterdemo/LoginActivity.kt
+++ app/src/main/java/com/loyer/twitterdemo/LoginActivity.kt
@@ -36,27 +36,60 @@ import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
-
-
-
-
-
+    private var mAuth: FirebaseAuth? = null
+    private var mEmail: EditText? = null
+    private var mPassword: EditText? = null
+    private var TAG = "Login"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-
+        mAuth = FirebaseAuth.getInstance()
+        mEmail = findViewById(R.id.edtEmail)
+        mPassword = findViewById(R.id.edtPassword)
 
     }
 
+    fun loginInExistingUser(view: View){
 
+        attemptLogin()
+    }
+
+
+    private fun attemptLogin(){
+        val email: String = mEmail!!.text!!.toString()
+        val pass: String = mPassword!!.text!!.toString()
+
+
+        if(email == "" || pass == "" ) return
+        Toast.makeText(this,"Login in progress...",Toast.LENGTH_SHORT).show()
+
+        mAuth!!.signInWithEmailAndPassword(email,pass).addOnCompleteListener { task ->
+            Log.d(TAG,"Login is succesfull " + task.isSuccessful)
+
+            if(!task.isSuccessful){
+                Log.d(TAG,"Same problem :"+ task.exception)
+                showErrorDialog("wrong email or password")
+            }else{
+                val intent = Intent(this@LoginActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
     fun txtRegister(view: View){
-       var intent = Intent(this@LoginActivity,RegisterActivity::class.java)
+       val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
         startActivity(intent)
         finish()
     }
 
 
-
+    private fun showErrorDialog(message: String) {
+        AlertDialog.Builder(this)
+                .setTitle("Oops!")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show()
+    }
 }
--- app/src/main/java/com/loyer/twitterdemo/MainActivity.kt
+++ app/src/main/java/com/loyer/twitterdemo/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.loyer.twitterdemo
 
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.ListView
+import com.loyer.twitterdemo.model.Post
+
 class MainActivity : AppCompatActivity() {
+
+    private var list = ArrayList<Post>()
+    private var mTweetList : ListView? = null
+    private var adapter: TweetListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mTweetList = findViewById(R.id.lstTweets)
+
+        //for adapter testing
+        list.add(Post("0","him","url","add"))
+        list.add(Post("0","him","url","loyer"))
+        list.add(Post("0","him","url","nLoyer"))
+
+         adapter = TweetListAdapter(list,this)
+         mTweetList!!.adapter = adapter
+
+
     }
+
 }
